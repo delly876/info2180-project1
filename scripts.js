5b29352ae45de2c4de807f2af87144df6b3e2454
Delano Senior <dsenior101@hotmail.com>
2020-10-24 13:24:54 -0500

    Edited CSS to fix placement of message div + Completed JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 /* Add your JavaScript to this file */
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var button = document.querySelector("button.btn");
+    var message = document.querySelector("div.message");
+    
+    button.addEventListener("click", function(e){
+        e.preventDefault();
+        getName();
+    })
+
+    function getName(){
+        var textfield = document.querySelector("#email").value;
+        if (textfield === ""){
+            message.innerHTML = "Please enter a valid email address.";
+        }
+        else{
+            message.innerHTML = "Thank You! Your email address " + textfield + " has been added to our mailing list!";
+        }
+        console.log(textfield);
+
+    }    
+});
